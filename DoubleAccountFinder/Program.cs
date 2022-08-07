614ed8dc45e83026fe972edb87221641c3220c10
Kasbolat Kumakhov <uhfath@gmail.com>
2022-08-07 15:15:26 +0300

    Add project files.

---
+++ DoubleAccountFinder/Program.cs
@@ -0,0 +1,14 @@
+﻿using OfficeOpenXml;
+
+namespace DoubleAccountFinder
+{
+	internal class Program
+	{
+		static int Main(string[] args)
+		{
+			ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+			return 0;
+		}
+	}
+}
