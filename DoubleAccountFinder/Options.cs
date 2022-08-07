6281251c713405019a498ccedff713c609d615ea
Kasbolat Kumakhov <uhfath@gmail.com>
2022-08-07 15:39:29 +0300

    Добавлен парсинг аргументов для поиска файлов/папок Добавлена возможность использования папки со всеми подпапками внутри Добавлена возможность конфига (ini)

---
+++ DoubleAccountFinder/Options.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleAccountFinder
+{
+	internal class Options
+	{
+		public int AccountColumn { get; set; }
+		public int AmountColumn { get; set; }
+		public string AccountRegex { get; set; }
+		public string Source { get; set; }
+	}
+}
--- DoubleAccountFinder/Program.cs
+++ DoubleAccountFinder/Program.cs
@@ -1,12 +1,49 @@
-﻿using OfficeOpenXml;
+﻿using Microsoft.Extensions.Configuration;
+using OfficeOpenXml;
 
 namespace DoubleAccountFinder
 {
 	internal class Program
 	{
+		const string SourceExtension = ".xlsx";
+
+		private static ISet<string> GetFiles(IEnumerable<string> sources) =>
+			sources
+				.Where(s => Directory.Exists(s))
+				.SelectMany(s => Directory.EnumerateFiles(s, SourceExtension, SearchOption.AllDirectories))
+				.Concat(sources
+					.Where(s => File.Exists(s)))
+				.Where(s => string.Equals(Path.GetExtension(s), SourceExtension, StringComparison.OrdinalIgnoreCase))
+				.ToHashSet()
+			;
+
 		static int Main(string[] args)
 		{
 			ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+			if (!args.Any())
+			{
+				Console.Error.WriteLine("Не указаны файлы для обработки");
+				return 1;
+			}
+
+			var sources = args
+				.Where(a => File.Exists(a) || Directory.Exists(a))
+			;
+
+			var files = GetFiles(sources);
+			if (!files.Any())
+			{
+				Console.Error.WriteLine("Указанные файлы недоступны или не в том формате");
+				return 2;
+			}
+
+			var configuration = new ConfigurationBuilder()
+				.AddIniFile("config.ini")
+				.Build()
+			;
+
+			var config = configuration.GetSection("Main").Get<Options>();
 
 			return 0;
 		}
